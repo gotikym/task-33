d3e8971fbba00e02f63c9ace00a697132e073aad
gotikym <gotikym9@mail.ru>
2022-07-24 19:39:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Shaffle(array);
+
+            Console.WriteLine("\nВывод после тасовки: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        static Random random = new Random();
+
+        static int[] Shaffle(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomNumber = i + (int)(random.NextDouble() * (array.Length - i));
+                int randomNumberFromArray = array[randomNumber];
+                array[randomNumber] = array[i];
+                array[i] = randomNumberFromArray;
+            }
+
+            return array;
+        }
+    }
+}
