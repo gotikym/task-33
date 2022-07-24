5cd6b58e174270ded74c8d2fe5634469bf50371c
Ivan Malkovskii <103892616+gotikym@users.noreply.github.com>
2022-07-24 20:38:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,35 +7,36 @@ namespace task_33
         static void Main(string[] args)
         {
             int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-            
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(array[i] + " ");
-            }
+
+            WriteArray(array);
 
             Shaffle(array);
 
             Console.WriteLine("\nВывод после тасовки: ");
 
+            WriteArray(array);
+        }
+
+        static void WriteArray(int[] array)
+        {
             for (int i = 0; i < array.Length; i++)
             {
                 Console.Write(array[i] + " ");
             }
         }
 
-        static Random random = new Random();
 
-        static int[] Shaffle(int[] array)
+        static void Shaffle(int[] array)
         {
+            Random random = new Random();
+
             for (int i = 0; i < array.Length; i++)
             {
                 int randomNumber = i + (int)(random.NextDouble() * (array.Length - i));
                 int randomNumberFromArray = array[randomNumber];
                 array[randomNumber] = array[i];
                 array[i] = randomNumberFromArray;
-            }
-
-            return array;
+            }   
         }
     }
 }
